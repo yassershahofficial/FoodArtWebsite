987d6d180b624e86cf6062441c0f99bcb301567b
yassershahofficial <geniususer4u@gmail.com>
2024-07-05 00:26:31 +0800

    fix express static & CSS Styling Home Section

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
@@ -6,7 +7,7 @@ const PORT = process.env.PORT || 3000
 
 app.set("views","../client/public/views")
 app.set("view engine", "ejs")
-app.use(express.static("public"))
+app.use(express.static("../client/public"))
 
 app.get('/', (req,res) => {
     res.render("index")
