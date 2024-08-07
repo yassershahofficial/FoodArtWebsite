0b81577421b38d557d04e7654b6908783be6cca4
yassershahofficial <geniususer4u@gmail.com>
2024-08-08 00:57:52 +0800

    nav & color theory3

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -2,7 +2,7 @@ console.log("hello world");
 
 const slideInRightTopRotate = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if(entry.isIntersecting){
             entry.target.classList.add('slideInRightTopRotate');
         } 
@@ -26,8 +26,16 @@ const hiddenL = document.querySelectorAll('.hiddenL');
 hiddenRTR.forEach((el) => slideInRightTopRotate.observe(el));
 hiddenL.forEach((el) => slideInLeft.observe(el));
 
-const navBtn = document.getElementById('header-ham');
+const headerHam = document.getElementById('header-ham');
+const headerMenu = document.getElementById('header-menu');
+const menu = document.getElementById('menu');
 
-navBtn.addEventListener("click", (e) => {
-    console.log("Pressed");
+headerHam.addEventListener("click", (e) => {
+    // console.log("Ham is Pressed");
+    menu.style.visibility = "visible";
+})
+
+headerMenu.addEventListener("click", (e) =>{
+    // console.log("Menu is clicked");
+    menu.style.visibility = "hidden";
 })
