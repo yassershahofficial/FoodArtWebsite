2b30276c6e944ba493206625a85628112268799e
yassershahofficial <geniususer4u@gmail.com>
2024-08-08 17:14:47 +0800

    updated design & optimization

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("hello world");
 
 const slideInRightTopRotate = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if(entry.isIntersecting){
             entry.target.classList.add('slideInRightTopRotate');
         } 
@@ -11,7 +11,7 @@ const slideInRightTopRotate = new IntersectionObserver((entries) => {
 
 const slideInLeft = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if(entry.isIntersecting){
             entry.target.classList.add('slideInLeft');
         } 
@@ -25,3 +25,21 @@ const hiddenRTR = document.querySelectorAll('.hiddenRTR');
 const hiddenL = document.querySelectorAll('.hiddenL');
 hiddenRTR.forEach((el) => slideInRightTopRotate.observe(el));
 hiddenL.forEach((el) => slideInLeft.observe(el));
+
+const headerHam = document.getElementById('header-ham');
+const headerMenu = document.getElementById('header-menu');
+const menu = document.getElementById('menu');
+
+headerHam.addEventListener("click", (e) => {
+    // console.log("Ham is Pressed");
+    menu.style.visibility = "visible";
+})
+
+headerMenu.addEventListener("click", (e) =>{
+    // console.log("Menu is clicked");
+    menu.style.visibility = "hidden";
+})
+
+function closeMenu(){
+    menu.style.visibility = "hidden";
+}
